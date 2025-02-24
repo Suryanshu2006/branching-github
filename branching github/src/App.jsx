20ecd1b8ec0dd5cbc9fbb889e6697f21e80b78eb
suryanshu2006 <singhsuryanshukumar5@gmail.com>
2025-02-24 17:27:44 +0530

    Updated

--- branching github/src/App.jsx
+++ branching github/src/App.jsx
@@ -1,8 +1,10 @@
-
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Maze from './components/Maze';
 import mazeData from './mazeData';
 import styled from 'styled-components';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import "./App.css";
 
 const PlayerStyled = styled.div`
   width: 30px;
@@ -12,47 +14,6 @@ const PlayerStyled = styled.div`
   top: ${(props) => props.y * 30}px;
   left: ${(props) => props.x * 30}px;
 `;
-
-function App() {
-  const [player, setPlayer] = useState({ x: 1, y: 1 }); // Starting position
-
-  const handleKeyDown = (e) => {
-    const { x, y } = player;
-    let newX = x;
-    let newY = y;
-
-    if (e.key === 'ArrowUp' && mazeData[y - 1][x] === 0) {
-      newY -= 1;
-    } else if (e.key === 'ArrowDown' && mazeData[y + 1][x] === 0) {
-      newY += 1;
-    } else if (e.key === 'ArrowLeft' && mazeData[y][x - 1] === 0) {
-      newX -= 1;
-    } else if (e.key === 'ArrowRight' && mazeData[y][x + 1] === 0) {
-      newX += 1;
-    }
-
-    setPlayer({ x: newX, y: newY });
-  };
-
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown);
-    return () => window.removeEventListener('keydown', handleKeyDown);
-  }, [player]);
-
-  return (
-    <div style={{ position: 'relative' }}>
-      <Maze />
-      <h1>Maze game</h1>
-      <PlayerStyled x={player.x} y={player.y} />
-    </div>
-  );
-}
-
-export default App;
-import React, { useState, useEffect } from "react";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import "./App.css";
 
 const AgeCalculator = () => {
   const [birthDate, setBirthDate] = useState("");
@@ -227,14 +188,42 @@ const TodoList = () => {
   );
 };
 
-const App = () => {
+function App() {
+  const [player, setPlayer] = useState({ x: 1, y: 1 }); // Starting position
+
+  const handleKeyDown = (e) => {
+    const { x, y } = player;
+    let newX = x;
+    let newY = y;
+
+    if (e.key === 'ArrowUp' && mazeData[y - 1][x] === 0) {
+      newY -= 1;
+    } else if (e.key === 'ArrowDown' && mazeData[y + 1][x] === 0) {
+      newY += 1;
+    } else if (e.key === 'ArrowLeft' && mazeData[y][x - 1] === 0) {
+      newX -= 1;
+    } else if (e.key === 'ArrowRight' && mazeData[y][x + 1] === 0) {
+      newX += 1;
+    }
+
+    setPlayer({ x: newX, y: newY });
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [player]);
+
   return (
-    <div>
+    <div style={{ position: 'relative' }}>
+      <Maze />
+      <h1>Maze game</h1>
+      <PlayerStyled x={player.x} y={player.y} />
       <AgeCalculator />
       <Quiz />
       <TodoList />
     </div>
   );
-};
+}
 
 export default App;
